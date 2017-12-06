14dd215b31ae012bbf7b91c60b0c5063bb102386
Hoidi <31310934+Hoidi@users.noreply.github.com>
2017-12-06 18:20:31 +0100

    da3

--- src/Day3.java
+++ src/Day3.java
@@ -1,6 +1,14 @@
 public class Day3 {
 
     public static void main(String[] args) {
+        int input = 265149;
 
+        int result1 = manhattanDistance(input);
+    }
+
+    private static int manhattanDistance(int input) {
+
+
+        return 0;
     }
 }
