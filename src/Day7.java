68f71bb4d21fbe8e465cfa58140f6130ec227758
Hoidi <31310934+Hoidi@users.noreply.github.com>
2017-12-07 09:59:06 +0100

    day7

---
+++ src/Day7.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        String[] input = {"pbga (66)","xhth (57)","ebii (61)","havc(66)","ktlj (57)"};
+
+        String result1 = bottomTower(input);
+
+        System.out.println(result1);
+    }
+
+    private static String bottomTower(String[] input) {
+
+
+        return null;
+    }
+}
