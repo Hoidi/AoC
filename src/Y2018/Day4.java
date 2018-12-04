ccd698a2b8f7af0ebcca8cc29204d5fb1c935d85
Hoidi <31310934+Hoidi@users.noreply.github.com>
2018-12-04 22:33:59 +0100

    Day4  started

--- src/Y2018/Day4.java
+++ src/Y2018/Day4.java
@@ -1,4 +1,34 @@
 package Y2018;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Day4 {
+
+    public static void main(String[] args) {
+        List<String> lines = null;
+        /*try {
+            lines = Files.readAllLines(Paths.get("D:\\Users\\Tobias\\Documents\\GitHub\\AoC\\src\\Y2018\\Day4.txt"), Charset.defaultCharset());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }*/
+
+        try {
+            lines = Files.readAllLines(Paths.get("C:\\Users\\Tobias\\Documents\\AoC\\src\\Y2018\\Day4Example.txt"), Charset.defaultCharset());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        lines.sort(String::compareTo);
+
+
+        System.out.println(sleepyTime(lines));
+    }
+
+    private static int sleepyTime(List<String> lines) {
+        return 0;
+    }
 }
