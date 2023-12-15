18bd14fe40074df3e2ed9e136e11e140df77e17b
Tobias Karlsson <github@hoidi.se>
2023-12-15 14:04:16 +0100

    Day 13 p2

--- Y2023/d13.py
+++ Y2023/d13.py
@@ -13,79 +13,96 @@ def printInput(input):
     print()
 
 
-def getMirrorIndexes(row: str):
+def getMirrorIndexes(row: str, possibleSym: []) -> []:
     sym = []
-    for step in range(len(row)):
-
+    for step in possibleSym:
         left = row[:step]
         right = row[step:]
-        # print("l:", left, "r:", right)
 
         maxLen = min(len(left), len(right))
 
         for i in range(maxLen):
-            # print(left[-(i+1)], right[i], i)
             if (left[-(i+1)] != right[i]):
                 break
 
             if i == maxLen-1:
-                # print("match", "l:", left, "r:", right)
                 sym.append(step)
-    # print(sym)
     return sym
 
 
 def keepMirrorIndex(possibleSym: [], newPossible: []) -> []:
-    # print("start ------------------")
-    # print(possibleSym, newPossible)
-    if (possibleSym == [-1]):
-        # print("init")
-        return newPossible
-
     result = list(newPossible)
     for i in newPossible:
         if i not in possibleSym:
-            # print(i, "not in", possibleSym, newPossible, "=>", result)
             result.remove(i)
-            # print(i, "not in", possibleSym, newPossible, "=>", result)
-
-    # print(result)
-    # print("end ------------------")
     return result
 
 
-# print(keepMirrorIndex([7], [3, 7]))
+def findSym(map, skipRow=-1, skipCol=-1):
+    possibleSym = [i for i in range(len(map[0]))]
+    if skipRow in possibleSym:
+        possibleSym.remove(skipRow)
 
-# exit()
+    for rowIndex, row in enumerate(map):
+        newPossible = getMirrorIndexes(row, possibleSym)
+        possibleSym = keepMirrorIndex(possibleSym, newPossible)
+
+    if (possibleSym != []):
+        return possibleSym[0], possibleSym[0], -1
+
+    map = np.transpose([list(col) for col in map])
+    possibleSym = [i for i in range(len(map[0]))]
+    if skipCol in possibleSym:
+        possibleSym.remove(skipCol)
+
+    for col in map:
+        newPossible = getMirrorIndexes(col, possibleSym)
+        possibleSym = keepMirrorIndex(possibleSym, newPossible)
+
+    if (possibleSym != []):
+        return 100 * possibleSym[0], -1, possibleSym[0]
+
+    return -1, -1, -1
+
+
+def flipped(map):
+    result = []
+    mapOg = map.copy()
+
+    for rowIndex, row in enumerate(map):
+        for colIndex, col in enumerate(row):
+            if col == ".":
+                changed = map[rowIndex][:colIndex] + \
+                    '#' + map[rowIndex][colIndex + 1:]
+                map[rowIndex] = changed
+            elif col == "#":
+                changed = map[rowIndex][:colIndex] + \
+                    '.' + map[rowIndex][colIndex + 1:]
+                map[rowIndex] = changed
+            result.append(map)
+            map = mapOg.copy()
+    return result
+
 
 part1 = 0
-for i, map in enumerate(maps):
-    # if (i != 1):
-    #    continue
+part2 = 0
 
-    possibleSym = [-1]
-    for rowIndex, row in enumerate(map):
-        newPossible = getMirrorIndexes(row)
-        possibleSym = keepMirrorIndex(possibleSym, newPossible)
-        # print(possibleSym)
+for mapI, map in enumerate(maps):
+    symForOgMap, r, c = findSym(map)
+    part1 += symForOgMap
 
-    # exit()
+    allFlipped = flipped(map)
 
-    if (possibleSym != [-1] and possibleSym != []):
-        # print("norm", possibleSym)
-        part1 += possibleSym[0]
-        continue
+    flippedSym = 0
+    for allFlippedMap in allFlipped:
+        symForFlippedMap, _, _ = findSym(allFlippedMap, r,   c)
 
-    possibleSym = [-1]
-    map = np.transpose([list(col) for col in map])
-    # printInput(map)
-    # print(map)
-    for col in map:
-        newPossible = getMirrorIndexes(col)
-        possibleSym = keepMirrorIndex(possibleSym, newPossible)
+        if symForFlippedMap == -1:
+            continue
+        flippedSym = symForFlippedMap
 
-    if (possibleSym != [-1] and possibleSym != []):
-        # print("trans", possibleSym)
-        part1 += 100 * possibleSym[0]
+    part2 += flippedSym
+
 
 print("Part 1:", part1)
+print("Part 2:", part2)
